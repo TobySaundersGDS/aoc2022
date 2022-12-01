b972a2b231cb541f3001b26e6ba77c86c351e9c5
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-12-01 12:10:32 +0000

    a basic ts solution

---
+++ day1.ts
@@ -0,0 +1,24 @@
+function day1part1(input: string){
+    let eachElf = input.split('\n\n');
+    let totalElf = Int32Array.from(eachElf.map(addTheElf));
+    totalElf = totalElf.sort();
+    totalElf = totalElf.reverse();
+    let max = totalElf[0]
+    let max3 = totalElf[0]+totalElf[1]+totalElf[2];
+    console.log(max);
+}
+function addTheElf( theElf: string): number{
+    let food = theElf.split('\n');
+    food.forEach(addTheFood)
+    return parseInt(food[0]);
+}
+function addTheFood(item, index, food){
+    if(index == 0){
+        food[0] = parseInt(item)
+    }else{
+        food[0] = food[0] + parseInt(item);
+    }
+}
+
+const input = process.argv[2];
+day1part1(input);
